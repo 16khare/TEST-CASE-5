cd80578c7f62e4689b63bf8ce6a84208f501d705
Chirag Khare <102572679+16khare@users.noreply.github.com>
2024-04-13 22:57:26 +0530

    UPDATE TOPOLOGICAL SORT

--- topological-sort.py
+++ topological-sort.py
@@ -10,17 +10,20 @@ G = nx.DiGraph()
 
 # Add nodes and edges to the graph
 for module, data in modules.items():
-    G.add_node(module)
+    G.add_node(module, path=data['path'])  # Add path information to the node
     for dependency in data['dependencies']:
         G.add_edge(dependency['artifactId'], module)
 
 # Perform topological sort
 sorted_modules = list(nx.topological_sort(G))
 
-# Print the sorted modules to a text file
-with open('sorted_modules.txt', 'w') as f:
+# Print the sorted modules and their paths to a text file
+with open('sorted_modules_with_paths.txt', 'w') as f:
     for module in sorted_modules:
-        f.write(module + '\n')
+        path = G.nodes[module]['path']  # Retrieve the path information for the module
+        f.write(f"{module} - Path: {path}\n")
+
+# You can also include additional information like build commands if needed
 
 
 
